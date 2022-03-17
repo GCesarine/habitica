f1cd803df04b6c02ce185689b1a24eab3ac4241b
Gregg CESARINE <gregg.cesarine@gmail.com>
2022-03-17 09:53:21 +0100

    Add base URL to axios

--- website/client/src/main.js
+++ website/client/src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import axios from 'axios';
 import AppComponent from './app';
 import {
   setup as setupAnalytics,
@@ -11,10 +12,10 @@ import StoreModule from './libs/store';
 import './filters/registerGlobals';
 import i18n from './libs/i18n';
 import 'smartbanner.js/dist/smartbanner';
-import axios from 'axios';
 
 const IS_PRODUCTION = process.env.NODE_ENV === 'production'; // eslint-disable-line no-process-env
 
+axios.defaults.baseURL = 'http://mediaserver/habitica';
 // Configure Vue global options, see https://vuejs.org/v2/api/#Global-Config
 
 // Enable perf timeline measuring for Vue components in Chrome Dev Tools
@@ -34,7 +35,6 @@ setUpLogging();
 setupAnalytics(); // just create queues for analytics, no scripts loaded at this time
 const store = getStore();
 
-axios.defaults.baseURL = 'http://mediaserver/habitica';
 
 export default new Vue({
   el: '#app',
